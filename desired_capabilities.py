ae425d964d054213939fb5d633f264c210bed323
Jakub Mareš <kubzej8@gmail.com>
2021-03-07 11:02:04 +0100

    real devices setup

--- desired_capabilities.py
+++ desired_capabilities.py
@@ -14,7 +14,9 @@ def get_desired_capabilities_android(no_reset):
         'platformName': 'Android',
         'app': s.app_android,
         'automationName': 'uiautomator2',
+        'deviceName': 'Android emulator',
         'noReset': no_reset,
+        'udid': "emulator-5554",
     }
     return desired_caps
 
@@ -25,6 +27,9 @@ def get_desired_capabilities_ios(no_reset):
         'app': s.app_ios,
         'automationName': 'XCUITest',
         'deviceName': 'iPhone 11',
+        'platformVersion': '14.4',
         'noReset': no_reset,
+        "xcodeOrgId": s.xcode_org_id,
+        "xcodeSigningId": s.xcode_signing_id
     }
     return desired_caps
